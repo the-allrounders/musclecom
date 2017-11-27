9336d184686c09cc716e1c76efd7c49dc0cb1d53
Bart Langelaan <langelaanbart@gmail.com>
2017-11-27 12:30:23 +0100

    Use hashrouter instead of browserrouter

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { AppContainer } from 'react-hot-loader';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import Root from './RootWrapper';
 
 const root = document.createElement('div');
@@ -11,9 +11,9 @@ document.body.appendChild(root);
 const mount = RootComponent => {
   render(
     <AppContainer>
-      <BrowserRouter>
+      <HashRouter>
         <RootComponent />
-      </BrowserRouter>
+      </HashRouter>
     </AppContainer>,
     root
   );
