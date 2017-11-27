098a32ee235dec80a3a25c1a2f61dc1d628c0bc3
Bart Langelaan <langelaanbart@gmail.com>
2017-11-27 12:53:12 +0100

    Add functions for sockets

--- src/server/middleware/sockets.js
+++ src/server/middleware/sockets.js
@@ -1,5 +1,21 @@
+let socket = null;
+
 export default function(io) {
-  io.on( "connection", () => {
-    // console.info(socket);
+  io.on('connection', newSocket => {
+    if(socket) console.warn('A new socket is overriding the other socket! Make sure you only have one connection.');
+    socket = newSocket;
+    socket.on('disconnect', () => {
+      if(socket !== newSocket) return;
+      console.warn('The socket is disconnected.');
+      socket = null;
+    });
   });
+}
+
+export function listen(...args) {
+  socket.listen(...args);
+}
+
+export function emit(...args) {
+  socket.emit(...args);
 }
