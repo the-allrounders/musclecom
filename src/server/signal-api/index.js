83983be941853c2a65650a5bea92cd2d53c5ea08
Menno van den Ende <mentosmenno2@hotmail.com>
2017-11-27 13:25:43 +0100

    fix(api): use existing socket
    fix: replace normal function with arrow function

--- src/server/signal-api/index.js
+++ src/server/signal-api/index.js
@@ -2,19 +2,13 @@
 
 import EventEmitter from 'events';
 import signalProcessing from '../signal-processing';
-
-const app = require('express')();
-const http = require('http').Server(app);
-const io = require('socket.io')(http);
-
-const port = process.env.PORT || 3000;
+import {emit, listen} from '../middleware/sockets';
 
 class SignalInterpretation extends EventEmitter {
 
   constructor() {
     super();
 
-    this.socket = null;
     this.numberOfSensors = 0;
 
     setupConnection();
@@ -22,34 +16,21 @@ class SignalInterpretation extends EventEmitter {
   }
 
   setupConnection() {
-    io.on("connection", function(socket){
-      console.log("User connected");
-      this.socket = socket;
-      this.socket.emit("numberOfSensors", this.numberOfSensors);
-
-      socket.on("disconnect", function(){
-        console.log("User disconnected");
-        this.socket = null;
-      });
-    });
+    emit("numberOfSensors", this.numberOfSensors);
   }
 
   addSPEventListeners() {
     signalProcessing.addListener("recievedSignal", (sensor, value) => {
       console.log("received signal", sensor, value);
       // mongodbmeuk
-      if(this.socket) {
-        this.socket.emit("receivedSignal", {sensor, value});
-      }
+      emit("receivedSignal", {sensor, value});
     });
 
     signalProcessing.addListener("numberOfSensors", (numSensors) => {
         console.log("number of sensors", numSensors);
         // mongodbmeuk
         this.numberOfSensors = numSensors;
-        if(this.socket) {
-          this.socket.emit("numberOfSensors", numberOfSensors);
-        }
+        emit("numberOfSensors", numberOfSensors);
     });
   }
 }
