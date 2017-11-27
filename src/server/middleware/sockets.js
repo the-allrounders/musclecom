cc54adffc8f50678e5fa1c2b596bb934f9f2c415
Bart Langelaan <langelaanbart@gmail.com>
2017-11-27 13:13:43 +0100

    Add onConnection exportable to sockets.js

--- src/server/middleware/sockets.js
+++ src/server/middleware/sockets.js
@@ -1,9 +1,45 @@
 let socket = null;
 
+const connectionListeners = [];
+
+const listeners = [];
+
+export function onConnection(func) {
+  connectionListeners.push(func);
+
+  // If an connection was already made, invoke the function directly.
+  if(socket) func();
+}
+
+export function listen(...args) {
+  listeners.push(args);
+  if(socket) socket.listen(...args);
+}
+
+const emitsToSend = [];
+export function emit(...args) {
+  if(socket) socket.emit(...args);
+  else emitsToSend.push(args);
+}
+
 export default function(io) {
   io.on('connection', newSocket => {
+
     if(socket) console.warn('A new socket is overriding the other socket! Make sure you only have one connection.');
+    else console.warn('Socket connected.');
+
     socket = newSocket;
+
+    // Send all emits that were sent to queue.
+    emitsToSend.forEach(emitArgs => socket.emit(...emitArgs));
+
+    // Invoke all listeners.
+    connectionListeners.forEach(listener => listener());
+
+    // Re-initiate all previously declared listeners
+    listeners.forEach(args => socket.listen(...args));
+
+    // On disconnect, remove the socket.
     socket.on('disconnect', () => {
       if(socket !== newSocket) return;
       console.warn('The socket is disconnected.');
@@ -12,10 +48,7 @@ export default function(io) {
   });
 }
 
-export function listen(...args) {
-  if(socket) socket.listen(...args);
-}
 
-export function emit(...args) {
-  if(socket) socket.emit(...args);
-}
+setInterval(() => {
+  emit('info', {numOfSensors: 2});
+}, 1000);
