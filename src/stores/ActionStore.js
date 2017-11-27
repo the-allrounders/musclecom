573b3d44e9b61ca89d4c86e9e253d8be2b389b15
Ian Wensink <ian@burst-digital.com>
2017-11-27 13:21:55 +0100

    feat(start-scene): add info listener to socket

--- src/stores/ActionStore.js
+++ src/stores/ActionStore.js
@@ -3,14 +3,24 @@ import io from 'socket.io-client';
 
 class ActionStore {
   @observable action = null;
+  @observable actionCount = 0;
+  @observable calibrationInfo = undefined;
+  @observable actions = [];
 
   constructor( ) {
     this.socket = io('http://localhost:6969');
     this.socket.on('action', this.setCurrentAction);
+    this.socket.on('info', this.updateInfo);
   }
 
   setCurrentAction = (action) => {
     this.action = action;
+  }
+
+  updateInfo = (newInfo) => {
+    this.actionCount = newInfo.actionCount;
+    this.calibrationInfo = newInfo.calibrationInfo;
+    this.actions = newInfo.actions;
   }
 }
 
