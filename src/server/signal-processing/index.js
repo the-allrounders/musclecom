dc2089f7c0b711ed9435c4f0279296ca3175f8e1
Raoul Vos <raoul-vos@hotmail.com>
2017-12-01 10:20:00 +0100

    channel scan working

--- src/server/signal-processing/index.js
+++ src/server/signal-processing/index.js
@@ -4,7 +4,7 @@ class SignalProcessing extends EventEmitter {
 
     async init() {
         return {
-            numOfSensors: this.numOfSensors,
+            numOfSensors: this.sensors.length,
         };
     }
 
@@ -19,51 +19,45 @@ class SignalProcessing extends EventEmitter {
         try {
             const Ads1x15 = require('node-ads1x15'); // eslint-disable-line global-require
             const adc = new Ads1x15(0);
-            console.info(adc);
             this.adc = adc;
         } catch(err) {
             console.info('It appears you are not running this on a Raspberry, I will feed you dummy data for the called functions');
             this.dummy = true;
         }
 
+        const setupCallback = (data) => {
+            console.info(data);
+            this.calibrateMin(1);
+            // setInterval(this.handleData, 1000);
+        };
+
         // Set initial number of signals / sensors.
-        this.numOfSensors = this.checkChannels();
-
-        // ** How to send a signal to the api.**
-        // @params
-        // string : eventName
-        // int : sensorNumber
-        // bool : activation
-        this.emit('recievedSignal', 3, true);
-
-        // ** Send a amount of sensors to set within the API. **
-        // @params
-        // string : eventName
-        // int : sensorNumber
-        this.emit('numberOfSensors', this.numOfSensors);
+        this.checkChannels(setupCallback);
     }
 
     /**
      * Read the channel for a single value
      * @param channel
      */
-     readChannel(channel) {
+    readChannel(channel, callback) {
+        console.info(`Reading channel: ${channel}`);
         if(!this.dummy && !this.adc.busy) {
             const samplesPerSecond = '250'; // see index.js for allowed values for your chip
             const progGainAmp = '4096'; // see index.js for allowed values for your chip
 
-            this.adc.readADCSingleEnded(channel, progGainAmp, samplesPerSecond, (err, data) => {
+            return this.adc.readADCSingleEnded(channel, progGainAmp, samplesPerSecond, (err, data) => {
                 if(err) {
                     // Log the error code
                     console.error(err);
                 }
 
-                console.info(`ChannelRead: ${channel} : ${data}`);
-                return data;
+                console.info(`Channel Read: ${channel} : ${data}`);
+                callback(data);
             });
         }
 
-        return false
+        console.info('Device busy');
+        return callback(false);
     }
 
     /**
@@ -71,32 +65,96 @@ class SignalProcessing extends EventEmitter {
      *
      * @returns {*}
      */
-    async checkChannels() {
+    checkChannels(callback) {
+        console.info('Checking number of sensors');
         if(!this.dummy) {
             // Create an array for our sensor
             this.sensors = [];
-            // Since we only use max 4 channels for our prototype we loop trough those 4 channels
-            const results = [];
-            for(let count = 0; count < 4; count += 1) {
-                // Set basic output to negative
-                // const callback = (value) =>{
-                //     if(value > 0) {
-                //         // value = -1 when adc is busy
-                //         // value = > 0 when value is returned
-                //         // value = 0 means no sensor is connected to this port
-                //         this.sensors.push(count);
-                //     }
-                // };
+            // Set a default counter
+            let count = 0;
 
-                // Start reading the channel and break only if a value has returned
-                results.push(this.readChannel(count));
-            }
-            console.info(await Promise.all(results));
-            return this.sensors.length;
+            // Set a private function to use as callbackg
+            /* eslint-disable */
+            const channelCallback = (value) => {
+                // if (!value) { // value == false when adc is busy
+                //     // Something happend retry the channel
+                //     this.readChannel(count, callback)
+                // } else
+                if(value > 0) {
+                    // value == > 0 when value is returned
+                    // value == 0 means no sensor is connected to this channel
+                    this.sensors.push({'channel':'count', 'min': 3000, 'max': 0});
+                }
+
+                // Up the counter
+                count += 1;
+
+                // Since we only have four sensors we break after this
+                if(count >= 4) {
+                    console.info(`Amount of sensors: ${this.sensors.length}`);
+
+                    // ** Send a amount of sensors to set within the API. **
+                    // @params
+                    // string : eventName
+                    // int : sensorNumber
+                    this.emit('numberOfSensors', this.sensors.length);
+
+                    return callback(this.sensors.length);
+                }
+
+                console.info(count);
+                // Start reading channels
+                this.readChannel(count, channelCallback)
+            };
+            /* eslint-enable */
+
+            // Start reading the channel and break only if a value has returned
+            this.readChannel(count, channelCallback)
         }
-
-        return SignalProcessing.getRandomInt(1,4);
+        return callback(SignalProcessing.getRandomInt(1, 4));
     };
+
+    calibrateMin(channel) {
+        let count = 0;
+        const objIndex = this.sensors.findIndex((obj => obj.channel === channel))
+        console.info(`objectIndex: ${objIndex}`);
+        const intervalID = setInterval(() => {
+            const callback = (value) => {
+                if(value > 0) {
+                    if(value < this.sensors[objIndex].min) {
+                        this.sensors[objIndex].min = value;
+                        console.info(`minval: ${value}`);
+                    }
+                }
+            };
+
+            this.readChannel(channel,callback);
+
+            if(count === 50) {
+                window.clearInterval(intervalID);
+            }
+            count += 1;
+        }, 100);
+    }
+
+    calibrateMax(channel) {
+        const objIndex = this.sensors.findIndex((obj => obj.channel === channel))
+        console.info(`Calibrating ${channel} on object ${objIndex}`);
+    }
+
+    handleData() {
+        const objIndex = this.sensors.findIndex((obj => obj.channel === channel))
+        console.info(`handling data on object ${objIndex}`);
+    }
+
+    /**
+     * Return the sensor array
+     *
+     * @returns {Array}
+     */
+    getSensors() {
+        return this.sensors;
+    }
 
     /**
      * Get a random integer value used for dummy data
