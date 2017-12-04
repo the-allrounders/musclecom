0c46ca37cb5918ef45e3eb8b7d9249e17301a54c
Bart Langelaan <langelaanbart@gmail.com>
2017-12-04 13:51:58 +0100

    Start mongodb on script start

--- scripts/start.sh
+++ scripts/start.sh
@@ -23,6 +23,28 @@ fi
 echo ""
 echo ""
 
+if hash mongod 2>/dev/null; then
+    echo "✅ Mongo is already installed."
+    echo "ℹ️ Your mongodb version information:"
+    mongod --version
+else
+    echo "ℹ️ Mongodb not yet installed, installing.."
+    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+    echo "deb http://repo.mongodb.org/apt/debian jessie/mongodb-org/3.4 main" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+    sudo apt-get update
+    sudo apt-get install -y mongodb-org
+fi
+
+echo ""
+echo ""
+
+echo "Starting mongo service.."
+
+sudo service mongod start
+
+echo ""
+echo ""
+
 echo "ℹ️ Installing yarn / checking for updates"
 npm install --global yarn@0
 
