309be600db52407619a4d6e5d4cc872cd8402bb6
Ian Wensink <ian@burst-digital.com>
2017-12-15 11:12:46 +0100

    feat(setup): add icons to sensor component

--- src/scenes/setup/SensorConnection/SensorConnectionItem/index.jsx
+++ src/scenes/setup/SensorConnection/SensorConnectionItem/index.jsx
@@ -1,13 +1,16 @@
 import React, { Fragment } from 'react';
 import { observer } from 'mobx-react';
+// svg
+import CheckIco from '../../../../vector/ic_check_24px.svg';
+import CloseIco from '../../../../vector/ic_close_24px.svg';
 
 const SensorConnectionItemComponent = ({ sensor }) => (
   <Fragment>
     <h3>Sensor {sensor.channel}</h3>
     <ul>
       <li>Channel: {sensor.channel}</li>
-      <li>Connected: {sensor.connected ? 'yes' : 'no'}</li>
-      <li>Calibrated: {sensor.calibrated ? 'yes' : 'no'}</li>
+      <li>Connected: {sensor.connected ? <CheckIco /> : <CloseIco />}</li>
+      <li>Calibrated: {sensor.calibrated ? <CheckIco /> : <CloseIco />}</li>
     </ul>
   </Fragment>
 );
