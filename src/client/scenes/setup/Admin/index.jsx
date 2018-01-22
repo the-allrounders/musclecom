7f6a3a0965629f6ef2bc3e8cf18dfe04c22e1027
Bart Langelaan <langelaanbart@gmail.com>
2018-01-22 22:27:01 +0100

    Always go from step 0 to step 1 when rendering on Remote

--- src/client/scenes/setup/Admin/index.jsx
+++ src/client/scenes/setup/Admin/index.jsx
@@ -1,27 +1,20 @@
 import React, { Component } from 'react';
 import { observer } from 'mobx-react';
 import { actionStore } from '../../../stores';
-
-// Components
 import ProgressBar from '../common/ProgressBar';
 import SensorStep from './SensorStep';
 import CalibrationStep from './CalibrationStep';
 import AppBar from '../common/AppBar';
-// styled
 import SetupWrapper from './styled/SetupWrapper';
 
 @observer
 class AdminSetupComponent extends Component {
-  componentDidMount() {
-    console.log('current step', actionStore.step);
-    if (actionStore.step === 0) {
-      console.log('Going to step 1!');
-      actionStore.setStep(1);
-    }
-  }
-
   render() {
     const { step } = actionStore;
+
+    if (actionStore.step === 0) {
+      actionStore.setStep(1);
+    }
 
     return (
       <SetupWrapper>
